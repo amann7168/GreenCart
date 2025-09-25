ee74ad81e9caa7b569e7c3bf6aafe3974ff54545
amann7168 <amann.rkee@gmail.com>
2025-09-26 00:15:30 +0530

    Created code layout

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Create context
+export const AppContext = createContext();
+
+// Context provider
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+
+  const value = { navigate, user, setUser, isSeller, setIsSeller };
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// // Custom hook to use context
+// export const useAppContext = () => {
+//   return useContext(AppContext);
+// };
